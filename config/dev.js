20ab3b089c139c2263ed58f16615b2277349898f
Fredy Ballesteros <fredy.ballesteros@gmail.com>
2019-10-31 15:39:04 -0500

    First commit

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb+srv://nodeuser:rTfE2hZ9c9JpEjff@cluster0-fwvar.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://localhost:6379'
+};
+
+
+
